b5287f7feae889509afe09341c0b15d62ea9fe44
zestiizo <zmaddix00@gmail.com>
2026-01-21 20:57:13 -0800

    pause menu wip

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -12,7 +12,7 @@ func _process(delta):
 
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/LevelOneTestPanel.tscn")
+	get_tree().change_scene_to_file("res://Scenes/MainLevel.tscn")
 
 
 func _on_settings_button_pressed():
