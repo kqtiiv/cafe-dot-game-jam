0998787de91b8b81b5385805cc770347de046a29
kqtiiv <katiewan.kw@gmail.com>
2026-01-26 00:19:08 +0000

    Add npc animation logic

--- Assets/3D/Sprites/character_rigged.gd
+++ Assets/3D/Sprites/character_rigged.gd
@@ -3,4 +3,5 @@ extends CharacterBody3D
 @onready var anim_tree = $AnimationTree
 
 func _ready():
-	anim_tree.set("parameters/BlendSpace1D/blend_position", 1.0)
+	#anim_tree.set("parameters/BlendSpace1D/blend_position", 1.0)
+	pass
--- path_follow.gd
+++ path_follow.gd
@@ -1,6 +1,27 @@
 extends PathFollow3D
 
 @export var speed: float = 2.0
+@export var sit_animation_name: String = "Sit"
 
-func _physics_process(delta):
-	progress += speed * delta
+@onready var anim_tree = $character_rigged/AnimationTree
+@onready var anim_player = $character_rigged/AnimationPlayer
+
+var has_sat_down: bool = false
+
+func _process(delta):
+	if progress_ratio < 1.0:
+		progress += speed * delta
+		anim_tree.set("parameters/BlendSpace1D/blend_position", 1.0)
+	else:
+		if not has_sat_down:
+			perform_sit_action()
+
+func perform_sit_action():
+	has_sat_down = true
+	print("Sitting down.")
+	
+	if anim_tree:
+		anim_tree.active = false
+		
+	if anim_player:
+		anim_player.play(sit_animation_name, 0.5)
