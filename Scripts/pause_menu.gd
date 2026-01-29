1bb3d1a8cd0f4fa02fedbd6de4c49c44e623534a
zestiizo <zmaddix00@gmail.com>
2026-01-29 00:02:51 -0800

    audio +
    more work on menu screens
    added first bits of audio

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@onready var audio = $Panel/VBoxContainer/button_click
+
 func _ready():
 	$AnimationPlayer.play("RESET")
 
@@ -19,19 +21,24 @@ func testEsc():
 
 
 func _on_resume_pressed():
+	audio.play()
 	resume()
 
 
 func _on_restart_pressed():
+	audio.play()
 	resume()
 	get_tree().reload_current_scene()
 
 
 func _on_settings_pressed():
-	pass # Replace with function body.
+	pass #connect OptionsUI here? or copy it I dunno >_<
+	
 
 
 func _on_quit_pressed():
+	audio.play()
+	await get_tree().create_timer(0.25).timeout
 	get_tree().quit()
 
 func _process(_delta):
