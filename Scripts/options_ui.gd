5489c0391eb19202e95350584cf3b1d152cc45da
kqtiiv <katiewan.kw@gmail.com>
2026-01-30 00:18:40 +0000

    add sound manager

--- Scripts/options_ui.gd
+++ Scripts/options_ui.gd
@@ -8,45 +8,49 @@ static var Instance: OptionsUI:
 @export var soundEffectsButton: Button
 @export var musicButton: Button
 @export var closeButton: Button
-@onready var audio = $Content/button_click
 
 var _onCloseButtonAction: Callable
 
 func _init() -> void:
-	if (_instance):
+	if _instance:
 		push_error("There is more than one OptionsUI instance")
 	_instance = self
 
 func _ready() -> void:
-	soundEffectsButton.pressed.connect(func():
-		audio.play()
-		SoundManager.Instance.ChangeVolume()
+	UpdateVisual()
+	
+	soundEffectsButton.pressed.connect(func(): 
+		SoundManager.ChangeVolume() 
 		UpdateVisual()
 	)
+	
 	musicButton.pressed.connect(func(): 
-		audio.play()
-		MusicManager.Instance.ChangeVolume()
+		MusicManager.ChangeVolume()
 		UpdateVisual()
 	)
+	
 	closeButton.pressed.connect(func(): 
-		audio.play()
 		hide()
-		_onCloseButtonAction.call()
+		if _onCloseButtonAction.is_valid():
+			_onCloseButtonAction.call()
 	)
 
 func _KitchenGameManager_OnGameUnpaused() -> void:
 	hide()
 
 func _exit_tree() -> void:
-	_instance = null
+	if _instance == self:
+		_instance = null
 
 func UpdateVisual() -> void:
-	soundEffectsButton.text = "Sound Effects: " + str(round(SoundManager.Instance.GetVolume() * 10.0))
-	musicButton.text = "Music: " + str(round(MusicManager.Instance.GetVolume() * 10.0))
+	if SoundManager:
+		soundEffectsButton.text = "Sound Effects: " + str(round(SoundManager.GetVolume() * 10.0))
+	
+	if MusicManager:
+		musicButton.text = "Music: " + str(round(MusicManager.GetVolume() * 10.0))
 
 func Show(onCloseButtonAction: Callable) -> void:
 	_onCloseButtonAction = onCloseButtonAction
-
+	UpdateVisual()
 	show()
-
 	soundEffectsButton.grab_focus()
